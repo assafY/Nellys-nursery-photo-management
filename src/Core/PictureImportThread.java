be32ef8368d61a6325b7ea092da2e130060d12d3
Ivaylo Kirilov <iikirilov@gmail.com>
2015-02-25 20:20:28 +0000

    mulithreaded import

---
+++ src/Core/PictureImportThread.java
@@ -0,0 +1,58 @@
+package Core;
+
+import Data.Picture;
+import GUI.MainFrame;
+
+import javax.swing.*;
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * Created by ivaylo on 25/02/15.
+ */
+public class PictureImportThread extends Thread {
+    File[] importedPictures;
+    ArrayList<Picture> picturesToDisplay = new ArrayList<Picture>();
+    public PictureImportThread(File[] importedPictures){
+        this.importedPictures = importedPictures;
+    }
+    public void run(){
+        try {
+            // for evey file path sent from importing in GUI
+            for (int i = 0; i < importedPictures.length; ++i) {
+
+                // check if it is already imported into library
+                boolean exists = false;
+                for (int j = 0; j < Library.getPictureLibrary().size(); ++j) {
+                    if (importedPictures[i].getPath().equals(
+                            Library.getPictureLibrary().get(j).getImagePath())) {
+                        exists = true;
+                    }
+                }
+                System.out.println(exists);
+                // if it doesn't exist in library
+                if (!exists) {
+                    // add picture to library
+                    Picture currentPicture = new Picture(
+                            importedPictures[i]);
+                    picturesToDisplay.add(currentPicture);
+                    System.out.println("Added: "
+                            + currentPicture.getImagePath());
+                }
+
+            }
+        } finally {
+            Runnable displayPictures = new Runnable() {
+
+                public void run() {
+                    System.out.println("Import Complete.");
+                    MainFrame.addThumbnailsToView(picturesToDisplay);
+                    for (int i = 0; i < picturesToDisplay.size(); ++i) {
+                        Library.addPictureToLibrary(picturesToDisplay.get(i));
+                    }
+                }
+            };
+            SwingUtilities.invokeLater(displayPictures);
+        }
+    }
+}
