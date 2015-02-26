975cb744d54094903dfcfcd437eb05727d1bc5c1
assafY <assaf.yossifoff@kcl.ac.uk>
2015-02-26 14:43:15 +0000

    minor

--- src/Core/Library.java
+++ src/Core/Library.java
@@ -3,19 +3,9 @@ package Core;
 import Data.Area;
 import Data.Child;
 import Data.Picture;
-import Data.Tag;
 import GUI.MainFrame;
-import GUI.PictureLabel;
-
-import javax.imageio.ImageIO;
-import javax.swing.*;
-
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.Serializable;
-import java.text.DateFormat;
-import java.text.FieldPosition;
-import java.text.ParsePosition;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
@@ -23,43 +13,43 @@ import java.util.Vector;
 
 public class Library implements Serializable {
 
-	private static ArrayList<Child> childrenList = new ArrayList<Child>();
-	private static Iterator childrenListIterator = childrenList.iterator();
-	private static ArrayList<Picture> pictureLibrary = new ArrayList<Picture>();
-	private static Iterator pictureLibraryIterator = pictureLibrary.iterator();
-	private static ArrayList<Area> areaList = new ArrayList<Area>();
-	private static Iterator areasListIterator = areaList.iterator();
-/**/	private static ArrayList<Date> possibleDate = new ArrayList<Date>();
-/**/	private static Iterator possibleDateIterator = possibleDate.iterator();
-	private static ArrayList<MainFrame> frames = new ArrayList<MainFrame>();
+    private static ArrayList<Child> childrenList = new ArrayList<Child>();
+    private static Iterator childrenListIterator = childrenList.iterator();
+    private static ArrayList<Picture> pictureLibrary = new ArrayList<Picture>();
+    private static Iterator pictureLibraryIterator = pictureLibrary.iterator();
+    private static ArrayList<Area> areaList = new ArrayList<Area>();
+    private static Iterator areasListIterator = areaList.iterator();
+    /**/    private static ArrayList<Date> possibleDate = new ArrayList<Date>();
+    /**/    private static Iterator possibleDateIterator = possibleDate.iterator();
+    private static ArrayList<MainFrame> frames = new ArrayList<MainFrame>();
 
-	public static void addFramesToNotify(MainFrame frame) {
-		frames.add(frame);
-	}
-	
-	public static synchronized ArrayList<Child> getChildrenList() {
-		return childrenList;
-	}
+    public static void addFramesToNotify(MainFrame frame) {
+        frames.add(frame);
+    }
+
+    public static synchronized ArrayList<Child> getChildrenList() {
+        return childrenList;
+    }
 
     public static synchronized ArrayList<Area> getAreaList() {
         return areaList;
     }
 
-	public static Vector<String> getChildrenNamesVector() {
-		Vector<String> childrenNames = new Vector<String>();
-		for (Child c : childrenList) {
-			childrenNames.add(c.getName());
-		}
-		return childrenNames;
-	}
+    public static Vector<String> getChildrenNamesVector() {
+        Vector<String> childrenNames = new Vector<String>();
+        for (Child c : childrenList) {
+            childrenNames.add(c.getName());
+        }
+        return childrenNames;
+    }
 
-	public static Vector<String> getAreaNamesVector() {
-		Vector<String> areaNames = new Vector<String>();
+    public static Vector<String> getAreaNamesVector() {
+        Vector<String> areaNames = new Vector<String>();
         for (Area a : areaList) {
             areaNames.add(a.getName());
         }
         return areaNames;
-	}
+    }
 
     public static Vector<String> getAllNamesVector() {
         Vector<String> allNames = getChildrenNamesVector();
@@ -71,58 +61,43 @@ public class Library implements Serializable {
 
     }
 
-	public static synchronized ArrayList<Picture> getPictureLibrary() {
-		return pictureLibrary;
-	}
+    public static synchronized ArrayList<Picture> getPictureLibrary() {
+        return pictureLibrary;
+    }
 
-	/* comment section */{
-		// public static void tagDate(ArrayList<Picture> picturesToTag, Date
-		// dateToTag) {
-		// //TO DO
-		// }
-		//
-		// public static void tagArea(ArrayList<Picture> picturesToTag, String
-		// Area) {
-		// //TO DO
-		// }
-		//
-		// public static void tagChildren(ArrayList<Picture> picturesToTag,
-		// ArrayList<String> childreninPicture) {
-		// //TO DO
-		// }
-	}
+    public static void importPicture(final File[] importedPictures) {
+        //specify no. of threads not including thread for leftover pictures
+        int noOfThreads = 10;
+        //size of array
+        int importSize = importedPictures.length;
+        //leftover calculated by size of array % no. of threads
+        int leftover = 0;
+        while (importSize % noOfThreads != 0 && importSize > noOfThreads) {
+            ++leftover;
+            --importSize;
+        }
+        //if there are more pictures than threads to import pictures
+        if (importedPictures.length > noOfThreads) {
+            //find out how many pictures will go in each thread and import
+            int chunkSize = importSize / noOfThreads;
+            for (int i = 0; i < importSize; i += chunkSize) {
+                File[] partition = new File[chunkSize];
+                System.arraycopy(importedPictures, i, partition, 0, chunkSize);
+                new PictureImportThread(partition).start();
+            }
+            //import leftover pictures
+            if (leftover > 0) {
+                File[] partialPartition = new File[leftover];
+                System.arraycopy(importedPictures, importedPictures.length - leftover, partialPartition, 0, leftover);
+                new PictureImportThread(partialPartition).start();
+            }
+        }
+        //if there are less pictures than threads to import pictures, import all pictures on 1 thread :))
+        else {
+            new PictureImportThread(importedPictures).start();
+        }
+    }
 
-	public static void importPicture(final File[] importedPictures) {
-		//specify no. of threads not including thread for leftover pictures
-		int noOfThreads = 10;
-		//size of array
-		int importSize = importedPictures.length;
-		//leftover calculated by size of array % no. of threads
-		int leftover = 0;
-		while(importSize % noOfThreads != 0 && importSize > noOfThreads){
-			++leftover;
-			--importSize;
-		}
-		//if there are more pictures than threads to import pictures
-		if(importedPictures.length > noOfThreads){
-			//find out how many pictures will go in each thread and import
-			int chunkSize = importSize/noOfThreads;
-			for(int i = 0; i<importSize; i+=chunkSize){
-				File[] partition = new File[chunkSize];
-				System.arraycopy(importedPictures,i,partition,0,chunkSize);
-				new PictureImportThread(partition).start();
-			}
-			//import leftover pictures
-			if(leftover > 0){
-				File[] partialPartition = new File[leftover];
-				System.arraycopy(importedPictures,importedPictures.length - leftover, partialPartition,0,leftover);
-				new PictureImportThread(partialPartition).start();
-			}
-		}
-		//if there are less pictures than threads to import pictures, import all pictures on 1 thread :))
-		else{
-			new PictureImportThread(importedPictures).start();
-		}
 
 		/*Thread newPictureImport = new Thread() {
 
@@ -171,35 +146,11 @@ public class Library implements Serializable {
 			}
 		};
 
-		newPictureImport.start();*/
-		// System.out.println("USED MEMORY: " +
-		// Runtime.getRuntime().totalMemory() + "FREE MEMORY: " +
-		// Runtime.getRuntime().freeMemory());
-	}
+		newPictureImport.start();
+	}*/
 
 	public static void importFolder(String folderPath) {
 		// TODO for every picture in folder do importPicture()
-	}
-
-	public static ArrayList<Picture> searchByChild(String childName) {
-		ArrayList<Picture> result = new ArrayList<Picture>();
-		// TODO
-		return result;
-	}
-
-	/* comment section */{
-
-		// public static ArrayList<Picture> searchByDate(Date date) {
-		// ArrayList<Picture> result = new ArrayList<Picture>();
-		// //TO DO
-		// return result;
-		// }
-		//
-		// public static ArrayList<Picture> searchByArea(String area) {
-		// ArrayList<Picture> result = new ArrayList<Picture>();
-		// //TO DO
-		// return result;
-		// }
 	}
 
 	public static void rotate(ArrayList<Picture> picturesToRotate,
@@ -235,26 +186,14 @@ public class Library implements Serializable {
 		areaList.remove(area);
 	}
 
-	
 	public static synchronized void addPictureToLibrary(Picture picture) {
 		pictureLibrary.add(picture);
 	}
-
-	
 	
 	public static void deletePictureLibrary()
 	{
 		pictureLibrary = null;
 	}
-
-//	public static void getCurrentMetadata() {
-//		if (selectedThumbs.size() <= 1) {
-//			// TODO: Show metadata on main frame
-//		} else {
-//			// TODO: compare metadata of all selected photos and show common
-//			// tags
-//		}
-//	}
 
 	public static Date getDate(String s) {
 		int date = Integer.parseInt(s.substring(0, 2));
