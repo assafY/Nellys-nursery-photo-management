2a21746541f18527b9ef171f66fe8bd3dc1fe5aa
assafY <assaf.yossifoff@kcl.ac.uk>
2015-02-26 00:49:21 +0000

    encapsulated methods of child and area in new taggable class

--- src/Data/Area.java
+++ src/Data/Area.java
@@ -1,38 +1,13 @@
 package Data;
 
 import Core.Library;
+import Core.Taggable;
 
-import java.util.ArrayList;
-
-/**
- * Created by assafyossifoff on 23/02/2015.
- */
-public class Area {
-
-    private String name;
-    private ArrayList<Picture> taggedPictures;
+public class Area extends Taggable{
 
     public Area(String name) {
-
-        this.name = name;
-        taggedPictures = new ArrayList<Picture>();
+        super(name);
         Library.addArea(this);
 
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void addTaggedPicture(Picture picture) {
-        taggedPictures.add(picture);
-    }
-
-    public void removeTaggedPicture(Picture picture) {
-        taggedPictures.remove(picture);
-    }
-
-    public ArrayList<Picture> getTaggedPictures() {
-        return taggedPictures;
     }
 }
