410ce104ba12687a3b1c792b2a273d19f2fa3071
assafY <assaf.yossifoff@kcl.ac.uk>
2015-03-11 00:24:58 +0000

    small thread class added. At the moment you need to select 'view all' in search to see all pictures at beginning

---
+++ src/Core/ThumbnailImportThread.java
@@ -0,0 +1,48 @@
+package Core;
+
+import Data.Picture;
+import GUI.MainFrame;
+import GUI.PictureLabel;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class ThumbnailImportThread extends Thread {
+
+    ArrayList<PictureLabel> picturesToDisplay = new ArrayList<PictureLabel>();
+    MainFrame mainFrame = MainFrame.getMainFrames().get(0);
+
+    public ThumbnailImportThread(ArrayList<PictureLabel> picturesToDisplay){
+        this.picturesToDisplay = picturesToDisplay;
+    }
+
+    public void run() {
+        try {
+            while (Settings.IMPORT_THREAD_COUNT > 10) {
+                sleep(500);
+            }
+            ++Settings.IMPORT_THREAD_COUNT;
+            // for evey thumbnail requesting view in GUI
+
+            for (PictureLabel p: picturesToDisplay) {
+                if (!mainFrame.getPicturesPanel().getThumbsOnDisplay().contains(p)) {
+                    p.createThumbnail();
+                    System.out.println("added: " + p.getPicture().getImagePath());
+                }
+            }
+
+        } catch (InterruptedException e) {
+
+        } finally {
+            /*Runnable displayPictures = new Runnable() {
+
+                public void run() {*/
+                    //mainFrame.getPicturesPanel().addThumbnailsToView(picturesToDisplay, MainFrame.getMainFrames().get(0).getZoomValue());
+                    --Settings.IMPORT_THREAD_COUNT;
+                /*}
+            };
+            SwingUtilities.invokeLater(displayPictures);*/
+
+        }
+    }
+}
